2199b63ac5f4684fb51d809d672e315c809a9f37
enmanuelrondon <enmanuelrondon54@gmail.com>
2025-11-24 15:54:51 -0400

    second commit

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -50,7 +50,7 @@ export function LoginForm() {
       const { error } = await supabase.auth.signInWithOAuth({
         provider: 'google',
         options: {
-          redirectTo: `${window.location.origin}/auth/callback`,
+         redirectTo: `${window.location.origin}/api/callback`,
         },
       });
   
